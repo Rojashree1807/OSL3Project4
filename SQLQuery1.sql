1401b84055054c33eda04aa680326e4c5108b4ac
Rojashree1807 <rojarojashree1807@gmail.com>
2024-01-19 12:05:52 +0530

    Initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+create database osl3pro3
+use osl3pro3
+create table feedback(
+Fname varchar(50),
+Lname varchar(50),
+Age int,
+Designation varchar(50),
+Feed nvarchar(255)
+)
+select * from feedback
---
+++ WebAppProject4/WebAppProject4/Feedback.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppProject4
+{
+    public partial class Feedback : System.Web.UI.Page
+    {
+       
+        static SqlConnection con;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Visible = false;
+        }
+
+        protected void Btnsub_Click(object sender, EventArgs e)
+        {
+            Label1.Visible = true;
+            try
+            {
+                con = new SqlConnection("Server=tcp:roja1812.database.windows.net,1433;Initial Catalog=osl3pro3;Persist Security Info=False;User ID=roja;Password=shree@2001;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                SqlCommand cmd = new SqlCommand("insert into feedback(Fname,Lname,Age,Designation,Feed) values(@fn,@ln,@age,@des,@feed)", con);
+                cmd.Parameters.AddWithValue("@fn", TextBox1.Text);
+                cmd.Parameters.AddWithValue("@ln", TextBox2.Text);
+                cmd.Parameters.AddWithValue("@age", int.Parse(TextBox3.Text));
+                cmd.Parameters.AddWithValue("@des", TextBox4.Text);
+                cmd.Parameters.AddWithValue("@feed", TextBox5.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Label1.Text = "Feedback submitted successfully ";
+
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = "error" + ex.Message;
+            }
+        }
+    }
+}
+
+
